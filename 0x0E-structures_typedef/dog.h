d421998aa64cb39d1cae6ff3fd91d1a814df5c67
Mugeez <brintonmgeez@gmail.com>
2023-06-19 11:13:33 -0700

    typedef project

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,22 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
 
-struct dog {
-    char* name;
-    float age;
-    char* owner;
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
-#endif 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
+#endif
